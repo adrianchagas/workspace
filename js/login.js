ae1e246332616fb2e05e5e67fd9860886f6a7331
adrianchagas <adrianchagas2014@gmail.com>
2021-09-02 13:35:40 -0300

    control login

--- js/login.js
+++ js/login.js
@@ -50,6 +50,6 @@ function signOut() {
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-    let botonSalir = document.getElementById("cerrarSesion");
-    botonSalir.style.display = "none";
+    //let botonSalir = document.getElementById("cerrarSesion");
+    //botonSalir.style.display = "none";
 });
