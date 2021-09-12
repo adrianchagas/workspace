6366b602f726d19eb9264d9eb20957ba2c41c8fa
adrianchagas <adrianchagas2014@gmail.com>
2021-09-12 12:29:50 -0300

    cambiando login

--- js/login.js
+++ js/login.js
@@ -1,8 +1,10 @@
+    
 function validar() {
     var usuario = document.getElementById('email');
     var contraseña = document.getElementById('pass');
     var span1 = document.getElementById('errorusuario');
     var span2 = document.getElementById('errorcontraseña');
+
     if (usuario.value === '' || contraseña.value === '') {
         span1.style.display = 'block';
         span2.style.display = 'block';
@@ -10,6 +12,8 @@ function validar() {
         span2.innerHTML = '¡Debe ingresar contraseña!';
         usuario.style.border = '2px solid red';
         contraseña.style.border = '2px solid red';
+
+   
     } else {
         sessionStorage.setItem('value', usuario.value);
         sessionStorage.setItem('value2', contraseña.value);
--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,91 @@
+var product = {};
+var comments = [];
+
+function showComment(){
+    let htmlContentToAppend = "";
+    for(let i = 0; i < comments.length; i++){
+        let comment = comments[i];
+        let estrella = `<span class="fa fa-star"></span>`;
+        let estrellaCheck = `<span class="fa fa-star checked"></span>`;
+        let calificacion = estrellaCheck.repeat(comment.score);
+        let estrellasSobran = estrella.repeat(5 -comment.score);
+        let mostrarCalificacion = calificacion + estrellasSobran;
+
+
+        htmlContentToAppend += `
+            <div class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h6 class="mb-1"><b>` + comment.user  +`</b></h6>
+                            <small class="text-muted"> `+ comment.dateTime +`</small>
+                        </div>
+                        <h6 class="mb-1">" ` + comment.description + ` "</h6>
+                        ` + mostrarCalificacion +`
+                        
+                    </div>
+                </div>
+            </div>
+        `
+        document.getElementById('comments').innerHTML = htmlContentToAppend;
+    }
+}
+
+function showImagesGallery(array){
+
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){
+        let imageSrc = array[i];
+
+        htmlContentToAppend += `
+        <div class="col-lg-3 col-md-4 col-6">
+            <div class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
+            </div>
+        </div>
+        `
+
+        document.getElementById("imagesGallery").innerHTML = htmlContentToAppend;
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            product = resultObj.data;
+
+            let productNameHTML  = document.getElementById("productName");
+            let productDescriptionHTML = document.getElementById("productDescription");
+            let productCostHTML = document.getElementById("productCost");
+            let productSoldCountHTML = document.getElementById('productSoldCount')
+            let productCategoryHTML = document.getElementById('productCategory')
+            let productRelatedProductHTML = document.getElementById("relatedProducts");
+        
+            productNameHTML.innerHTML = product.name;
+            productDescriptionHTML.innerHTML = product.description;
+            productCostHTML.innerHTML =  product.cost + ' ' + product.currency;
+            productSoldCountHTML.innerHTML = product.soldCount;
+            productCategoryHTML.innerHTML = product.category;
+            productRelatedProductHTML.innerHTML = product.relatedProducts;
+
+            //Muestro las imagenes en forma de galería
+            showImagesGallery(product.images);
+        }
+    });
+
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            comments = resultObj.data;
+
+            //Muestro los comentarios
+            showComment();
+        }
+    });
 
 });
