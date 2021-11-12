585c9c7fe6e2377890951832f663dbb099c7631b
adrianchagas <adrianchagas2014@gmail.com>
2021-11-12 14:41:16 -0300

    entrega 7

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,5 @@
 var cart = {};
+var mensaje = {};
 let costoEnv = 0;
 
 function showProductCart(array){
@@ -64,6 +65,8 @@ function formaDePago(){
         <label for="tarjeta">Código de verificación:</label>
         <input type="text" id="verificacion" placeholder="Ingresar fecha de vencimiento" onblur="cargarErrores('verificacion', 'opcion6')">
         <p><span id="opcion6" style="color: red;"></span></p>
+        <button onclick="erroresPagoCredito();" type="button" class="btn btn-primary">Continuar</button>
+
         `
         document.getElementById('mostrar-forma-pago').innerHTML = htmlContentToAppend;
         }
@@ -82,8 +85,9 @@ function formaDePago(){
         </select>
         <br>
         <label for="cuenta">Número de cuenta:</label>
-        <input type="text" id="cuenta" placeholder="Ingresar número de cuenta" onblur="cargarErrores('cuenta', 'opcion7')">
+        <input type="text" id="cuenta" value="" placeholder="Ingresar número de cuenta" onblur="cargarErrores('cuenta', 'opcion7')">
         <p><span id="opcion7" style="color: red;"></span></p>
+        <button onclick="errorBanco();" type="button" class="btn btn-primary">Continuar</button>
         `
         document.getElementById('mostrar-forma-pago').innerHTML = htmlContentToAppend;
     }
@@ -140,37 +144,74 @@ function contenido(){
         document.getElementById('cont').innerHTML=cantidad;
 }
 
+function erroresPagoCredito(){
+    var tarjeta = document.getElementById('tarjeta');
+    var vencimiento = document.getElementById('vencimiento');
+    var verificacion = document.getElementById('verificacion');
+    let errorPago = document.getElementById('errorPago');
+    let correct1 = document.getElementById('correct1');
+
+    if(tarjeta.value === "" || vencimiento.value === "" || verificacion.value === ""){
+        correct1.style.display = 'none';
+        errorPago.style.display = 'block';
+        errorPago.innerHTML= "¡Error! Para poder continuar, debe completar todos los campos!";
+        document.getElementById('tarjeta').classList.add('error');
+        document.getElementById('vencimiento').classList.add('error');
+        document.getElementById('verificacion').classList.add('error');
+    } else {
+        document.getElementById('tarjeta').classList.remove('error');
+        document.getElementById('vencimiento').classList.remove('error');
+        document.getElementById('verificacion').classList.remove('error');
+        errorPago.style.display = 'none';
+        correct1.style.display = 'block';
+        correct1.innerHTML = "¡Listo! Puede cerrar la ventana y continuar...";
+    }
+    }
+
+function errorBanco(){
+    var cuenta = document.getElementById('cuenta');
+    let errorPago = document.getElementById('errorPago');
+    let correct1 = document.getElementById('correct1');
+    let banco = document.getElementById('banco').value;
+
+    if (cuenta.value == "" || banco == 'Elegir'){
+        correct1.style.display = 'none';
+        errorPago.style.display = 'block';
+        errorPago.innerHTML= "¡Error! Para poder continuar, debe seleccionar un banco e ingresar número de cuenta!";
+        document.getElementById('cuenta').classList.add('error');
+    }else{
+        document.getElementById('cuenta').classList.remove('error');
+        errorPago.style.display = 'none';
+        correct1.style.display = 'block';
+        correct1.innerHTML = "¡Listo! Puede cerrar la ventana y continuar...";
+    }
+}
 
 function errores(){
     var calle = document.getElementById('calle').value;
     var numero = document.getElementById('numero').value;
     var esquina = document.getElementById('esquina').value;
-    var tarjeta = document.getElementById('tarjeta').value;
-    var vencimiento = document.getElementById('vencimiento').value;
-    var verificacion = document.getElementById('verificacion').value;
+    
     let error = document.getElementById('error');
-    let correct = document.getElementById('correct');
+    //let correct = document.getElementById('correct');
 
-    if (calle === "" || numero === "" || esquina === "" || tarjeta === "" || vencimiento === "" || verificacion === ""){
+    if (calle == "" || numero == "" || esquina == ""){
         error.style.display = 'block';
         error.innerHTML = "¡ERROR! ¡Para finalizar el proceso de compra, verifique que haya completado todos los campos, que haya seleccionado un tipo de envío y una forma de pago! ";
         document.getElementById('calle').classList.add('error');
         document.getElementById('numero').classList.add('error');
         document.getElementById('esquina').classList.add('error');
-        document.getElementById('tarjeta').classList.add('error');
-        document.getElementById('vencimiento').classList.add('error');
-        document.getElementById('verificacion').classList.add('error');
+        
 
     } else {
         document.getElementById('calle').classList.remove('error');
         document.getElementById('numero').classList.remove('error');
         document.getElementById('esquina').classList.remove('error');
-        document.getElementById('tarjeta').classList.remove('error');
-        document.getElementById('vencimiento').classList.remove('error');
-        document.getElementById('verificacion').classList.remove('error');
+       
 
         error.innerHTML = '';
-        correct.innerHTML = "¡¡¡ Su compra ha sido generada con éxito !!!"
+        mostrarMsjCompra();
+        
     }
 }
 
@@ -205,12 +246,44 @@ function cargarErrores(id, idMensaje) {
     if(elementos.value==''){
         elementError.style.display = "block";
         elementError.innerHTML = "Debe ingresar " + elementos.id + "!";
-         elementNombre.classList.add("error");
+         //elementNombre.classList.add("error");
     }
     else {(elementos.classList.remove("error"));
     elementError.innerHTML="";
  }
 }
+
+function mostrarMsjCompra(){
+    let htmlContentToAppend =  "";
+    
+    htmlContentToAppend = `
+    <div class="modal fade" id="mensajeCompra" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+    <div class="modal-header">
+    <h5 class="modal-title" id="exampleModalLabel">Compra finalizada</h5>
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+    </div>
+    <div class="modal-body">
+    <div class="text-success">`+ mensaje + `</div>
+    </div>
+    <div class="modal-footer">
+    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+    </div>
+    </div>
+    </div>
+    </div>
+    `
+
+document.getElementById('mostrarMsj').innerHTML = htmlContentToAppend;
+
+}
+
+$('#medioPago').on('shown.bs.modal', function () {
+    $('#tarjeta').trigger('focus')
+})
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -221,9 +294,17 @@ document.addEventListener("DOMContentLoaded", function(e){
             cart = resultObj.data;
         }
 
+
         contenido();
         showProductCart(cart.articles);
         subTotal();
         activarBoton();
     });   
+
+    getJSONData(CART_BUY_URL).then(function(resultObj) {
+        if (resultObj.status === "ok") {
+            mensaje = resultObj.data.msg;
+            console.log(mensaje);
+    }
+ });
 });
